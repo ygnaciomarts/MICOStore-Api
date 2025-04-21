b915636d27a72c6fb7aa90b53273654c44c5c55e
Ygnacio Martínez Sánchez <95099168+ygnaciomarts@users.noreply.github.com>
2025-04-20 18:11:57 -0600

    Allow connections from all endpoints

--- src/main/java/org/mico/micostoreapi/config/SecurityConfig.java
+++ src/main/java/org/mico/micostoreapi/config/SecurityConfig.java
@@ -25,9 +25,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/api/products/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
