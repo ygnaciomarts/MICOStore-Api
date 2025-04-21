2a6f91121539b58f0feec609ae4870a6af771718
Ygnacio Martínez Sánchez <95099168+ygnaciomarts@users.noreply.github.com>
2025-04-20 18:18:27 -0600

    Change AllowedCors config

--- src/main/java/org/mico/micostoreapi/config/CorsConfig.java
+++ src/main/java/org/mico/micostoreapi/config/CorsConfig.java
@@ -14,9 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
         };
